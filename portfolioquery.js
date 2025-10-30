f708301a67218a9f37411bbe55fbb5c29220a83b
Anish S <143774931+leoanish007@users.noreply.github.com>
2025-10-30 13:36:31 +0530

    Add files via upload

---
+++ portfolioquery.js
@@ -0,0 +1,48 @@
+$(document).ready(function(){
+    $("#btn-e").click(function(){
+      $(".frame").toggle(400);
+
+
+});
+   // $("#btn-e").mouseleave(function(){
+   //   $(".frame").hide(400)});
+
+
+
+
+
+    $("#btn-p").click(function(){
+
+        $(".projects").toggle(400);
+    });
+
+    //$("#btn-p").mouseleave(function(){        $(".projects").hide(400);});
+
+
+
+
+    $("#btn-c").click(function(){
+        $(".certifications").toggle(400);
+        
+    });
+   // $("#btn-c").mouseleave(function(){$(".certifications").hide(400);});
+
+
+
+
+    $("#btn-co").click(function(){
+        $(".contact").toggle(400);
+
+    });
+
+
+
+
+
+
+
+
+
+
+  
+});
